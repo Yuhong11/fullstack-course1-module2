d950c3255be2c71ab1bb34e799778a5edf05aa46
Yuhong Liu <yliu556@gmail.com>
2017-01-03 01:29:49 -0800

    fixed his code in Lec15

--- Lecture15-IntroToTesting/calculator_test.rb
+++ Lecture15-IntroToTesting/calculator_test.rb
@@ -6,6 +6,18 @@ class CalculatorTest < Test::Unit::TestCase
   	@calc = Calculator.new('test')
   end
 
+  def test_addition
+  	assert_equal 4, @calc.add(2,2)
+  end
+
+  def test_subtration
+  	assert_equal 2, @calc.subtract(4,2)
+  end
+
+  def test_division
+  	assert_equal 2, @calc.divide(4,2)
+  end
+
   def test_divide_by_zero
     assert_raise ZeroDivisionError do 
       @calc.divide(1, 0)
